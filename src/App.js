d6dc939ca054eef442b97c1f47f83157461bcc4c
Yash Utkarsh <yash.u@codeparva.in>
2021-05-22 22:48:28 +0530

    Initial; Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,163 @@
-import logo from './logo.svg';
+import logo from './heroimage@2x.png';
+import icon1 from './freshworks.png'
+import icon2 from './goop.png'
+import icon3 from './study.png'
+import newPricingIcon from './crack-new-segments.svg'
+import sbSectionImage from './sb-section-console.svg'
+import automationImage from './subscription-automation.svg'
+import billingIcon from './billing-experiments.svg'
+import integration from './integration.svg'
+import { Grid, Button } from './Components';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container justify="space-between">
+        <Grid container className="actionItems" spacing={3} alignItems="flex-end">
+          <Grid item>
+            <p className="logo">Chargebee</p>
+          </Grid>
+          {["Product", "Pricing", "Solutions", "Customers", "Resources"].map(label => (
+            <Grid item key={label}>
+              <p className="actionItem">{label}</p>
+            </Grid>
+          ))}
+        </Grid>
+        <Grid container spacing={3} className="headerActions">
+          <Grid item>
+            <Button classes={{ label: "scheduleDemoLabel" }}>{"Schedule a Demo"}</Button>
+          </Grid>
+          <Grid item>
+            <Button
+              className="logInButton"
+              variant="contained"
+              classes={{ label: "logInButtonLabel" }}
+            >
+              {"Log In"}
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+      <p className="subscription">{"Subscription Billing & Revenue Operations"}</p>
+      <p className="business">{"For Business\nDesigned for Growth"}</p>
+      <p className="subText">{"Chargebee gives you the freedom to deploy the right\ngrowth strategies, while automating everything else in\nyour order-to-revenue workflow."}</p>
+      <Grid container className="buttons" spacing={2}>
+        <Grid item>
+          <Button
+            variant="contained"
+            className="getButton"
+            classes={{ label: "getButtonLabel" }}
+          >
+            Get a Consultation
+              </Button>
+        </Grid>
+        <Grid item>
+          <Button
+            variant="contained"
+            className="signUpButton"
+            classes={{ label: "signupButtonLabel" }}
+          >
+            Signup for Free
+            </Button>
+        </Grid>
+      </Grid>
+      <p className="trustedText">{"Trusted by leading brands to process over\n$3Bn in recurring revenue."}</p>
+      <Grid container justify="space-between" className="icons">
+        <Grid item xs={2}>
+          <img src={icon2} alt="icon2" className="icon" />
+        </Grid>
+        <Grid item xs={3}>
+          <img src={icon3} alt="icon3" className="icon" />
+        </Grid>
+        <Grid item xs={3}>
+          <img src={icon1} alt="icon1" className="icon" />
+        </Grid>
+      </Grid>
+
+
+      <Grid container className="section2">
+        <Grid container item className="subsection1" direction="column">
+          <Grid container>
+            <Grid item className="block" />
+            <p className="deployText">{"DEPLOY STRATEGIES"}</p>
+          </Grid>
+          <p className="challengeText">{"What does this\nchallenge make\npossible?"}</p>
+          <p className="section2Text">{"A changing world requires an evolving\nbusiness strategy. Chargebee lets you\nadapt business models and deploy\nstrategies as fast as you can think of them."}</p>
+        </Grid>
+        <Grid container item className="subsection2" alignItems="center" justify="space-between">
+          <Grid container className="box rotate" direction="column">
+            {/* <img src={newPricingIcon} alt="icon1" className="newPricingIcon" />
+            <p className="rollText">{"Roll"}</p>
+            <p className="newPricingLabel">New Pricing</p>
+            <p className="boxDescriptionText">{"Improve value-price fit.\nIterate and launch new\npricing structures without\nthe dev effort."}</p>
+            <Button className="boxSubmit" classes={{ label: "boxSubmitLabel" }}>{"Change Pricing"}</Button> */}
+          </Grid>
+          <Button className="arrowButton" classes={{ label: "arrowButtonLabel" }}>{"<"}</Button>
+          <Grid container className="box middleBox" direction="column">
+            <img src={newPricingIcon} alt="icon1" className="newPricingIcon" />
+            <p className="rollText">{"Roll"}</p>
+            <p className="newPricingLabel">New Pricing</p>
+            <p className="boxDescriptionText">{"Improve value-price fit.\nIterate and launch new\npricing structures without\nthe dev effort."}</p>
+            <Button className="boxSubmit" classes={{ label: "boxSubmitLabel" }}>{"Change Pricing"}</Button>
+          </Grid>
+          <Button className="arrowButton" classes={{ label: "arrowButtonLabel" }}>{">"}</Button>
+          <Grid container className="box rotate" direction="column">
+            {/* <img src={newPricingIcon} alt="icon1" className="newPricingIcon" />
+            <p className="rollText">{"Roll"}</p>
+            <p className="newPricingLabel">New Pricing</p>
+            <p className="boxDescriptionText">{"Improve value-price fit.\nIterate and launch new\npricing structures without\nthe dev effort."}</p>
+            <Button className="boxSubmit" classes={{ label: "boxSubmitLabel" }}>{"Change Pricing"}</Button> */}
+          </Grid>
+          <Grid item className="sbSectionImage">
+            <img src={sbSectionImage} alt="logo2" />
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid container className="section2">
+        <Grid container item className="subsection1 noTopPadding" direction="column">
+          <Grid container>
+            <Grid item className="block" />
+            <p className="deployText">{"BEYOND BILLING "}</p>
+          </Grid>
+          <p className="challengeText">{"Streamline Your\nEntire RevOps\nWorkflow"}</p>
+          <p className="section2Text">{"The next big revenue opportunity\nneed not be familiar. Chargebee\ngives you the infrastructure to\ndiscover and turn every revenue\npotential into a business outcome."}</p>
+        </Grid>
+        <Grid container className="section3subsection2" direction="column">
+          <Grid container justify="space-between" className="headerTextContainer">
+            <p className="section3HeaderText selected">{"Subscription\nAutomation\n\n"}</p>
+            <p className="section3HeaderText">{"Billing\nExperiments\n\n"}</p>
+            <p className="section3HeaderText">{"Extensible\nPlatform\n\n"}</p>
+          </Grid>
+          <Grid container>
+            <img src={automationImage} alt="logo3" className="automationImage" />
+            <Grid container className="automationDescriptionBox" direction="column">
+              <p className="automateText">{"Automate the\nOrder-to-Revenue\nLifecycle"}</p>
+              <p className="automationDescriptionText">{"Collect recurring payments\nand automate your subscription\nbilling logic - from checkout to\nreconciliation. "}</p>
+              <Button className="boxSubmit" classes={{ label: "boxSubmitLabel" }}>{"Subscription Automation"}</Button>
+            </Grid>
+          </Grid>
+          <Grid container className="thirdSectionSubs">
+            <img src={billingIcon} alt="logo3" className="automationImage" />
+            <Grid container className="automationDescriptionBox" direction="column">
+              <p className="automateText">{"Evolve your Billing\nWorkflow to Fit Your\nBusiness Strategy"}</p>
+              <p className="automationDescriptionText">{"Adjust Pricing Structures, product\nbundles, acquisition flows and\nrecovery processes to maximize\nrevenue."}</p>
+              <Button className="boxSubmit" classes={{ label: "boxSubmitLabel" }}>{"Billing Experiments"}</Button>
+            </Grid>
+          </Grid>
+          <Grid container className="thirdSectionSubs">
+            <img src={integration} alt="logo3" className="automationImage" />
+            <Grid container className="automationDescriptionBox" direction="column">
+              <p className="automateText">{"Scale Your Revenue Stack\nfor Each Stage of Growth"}</p>
+              <p className="automationDescriptionText">{"Upgrade finance,\naccounting, CRM, and more\non the revenue engine that\nscales with you. "}</p>
+              <Button className="boxSubmit" classes={{ label: "boxSubmitLabel" }}>{"Extensible Platform"}</Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      
+      <img src={logo} alt="logo2" width="1920" height="1080" className="heroImage" />
     </div>
   );
 }
